725c789f5a2830c8f5ac58e5cadc6fc7a92f3e0c
Gaspard MICHEL <gaspard.michel@insa-lyon.fr>
2021-05-04 21:48:19 +0200

    Interaction part 1

--- Assets/Game/Scripts/PlayerMouvement.cs
+++ Assets/Game/Scripts/PlayerMouvement.cs
@@ -2,14 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
+using static Interactable;
 
 public class PlayerMouvement : NetworkBehaviour
 {
+	
 	[SyncVar]
     public float speed;
+    //[SyncVar]
+    public Interactable focus;
     private Rigidbody2D myRigidbody;
     private Vector3 change;
     private Animator animator;
+    private RaycastHit hit;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,10 +25,33 @@ public class PlayerMouvement : NetworkBehaviour
     // Update is called once per frame
     void Update()
     {
-            change = Vector3.zero;
-            change.x = Input.GetAxisRaw("Horizontal");
-            change.y = Input.GetAxisRaw("Vertical");
-            UpdateAnimationAndMove();
+        change = Vector3.zero;
+        change.x = Input.GetAxisRaw("Horizontal");
+        change.y = Input.GetAxisRaw("Vertical");
+        UpdateAnimationAndMove();
+
+        //test pour une interaction avec la touche E
+        if(Input.GetKey(KeyCode.E)){
+        	
+        	
+
+           	//if(Physics.Raycast(change, hit, 100)){
+
+        		Interactable interactable = hit.collider.GetComponent<Interactable>();
+
+           		if (interactable != null){
+
+           			SetFocus(interactable);
+
+            	}
+            }
+        //}
+    }
+
+    void SetFocus(Interactable newFocus){
+
+    	focus = newFocus;	
+
     }
 
     void UpdateAnimationAndMove(){
