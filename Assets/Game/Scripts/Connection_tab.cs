1ff8d8d60a1b5e31a972fde801eaac2210c28186
mathisfchx <72497356+mathisfchx@users.noreply.github.com>
2021-05-26 11:19:12 +0200

    Equipes et couleurs fonctionnelles
    un perso peut avoir une équipe, son pseudo s'actualise bien à la bonne couleur, seulement il reste des problemes lors du calcul du nombre de joueur dans chaque team, je n'arrive donc pas à déclencher le StartGame

--- Assets/Game/Scripts/Connection_tab.cs
+++ Assets/Game/Scripts/Connection_tab.cs
@@ -10,7 +10,28 @@ public class Connection_tab : NetworkBehaviour
     [SyncVar]
     public int conn_id = 0; 
     public SyncDictionary<string, int> conn_tab = new SyncDictionary<string, int>();
+    [SyncVar]
+    public int nTeamA;
+    [SyncVar]
+    public int nTeamB;
+    [SyncVar]
+    public int start;
 
+void Start()
+{
+    CmdResetStart();
+}
+
+void Update()
+{
+    if (nTeamA == 1 && nTeamB == 1)
+    {
+        if (hasAuthority){
+            start = 1;
+        }
+        
+    }
+}
 
 [Command]
 public void CmdSetDico(string username, int conn_id)
@@ -22,6 +43,12 @@ public void CmdSetDico(string username, int conn_id)
     //RpcModifDico(username, conn_id);
     //}
 
+}
+
+[Command]
+public void CmdResetStart()
+{
+    start = 0;
 }
 
 }
---
+++ Assets/Game/Scripts/script.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class script : NetworkBehaviour
+{
+    [SyncVar]
+    public int start;
+    [SerializeField] public Connection_tab conn_tab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        start = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (hasAuthority){
+            CmdSetStart();
+            if (start == 1){
+                Debug.Log("Commencer la partie");
+                CmdStartGame();
+            }
+        }
+    }
+
+    [Command]
+    public void CmdStartGame()
+    {
+        Debug.Log("cmd start game");
+        //gérer position des perso pour les faire spawn chez eux (délire de positions en x,y)
+        //mais spawner les objets (voir du coté de network manager peut etre je suis aps sur)
+        //lancer les règles ()
+    }
+
+    [Command]
+    public void CmdSetStart()
+    {
+        start = conn_tab.start;
+        RpcSetStart();
+    }
+
+    [ClientRpc]
+    public void RpcSetStart()
+    {
+        start = conn_tab.start;
+    }
+}
